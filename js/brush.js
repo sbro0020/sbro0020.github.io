a98df5c31ee02f9e4e9b33dc7dcc8d9cbd78ac1d
Byrix <brosea101@gmail.com>
2021-10-16 17:55:24 +1100

    awd

---
+++ js/brush.js
@@ -0,0 +1,52 @@
+var styles = window.getComputedStyle(document.getElementById('brush'))
+var containerWidth = parseFloat(styles.width);
+var containerHeight = parseFloat(styles.height);
+
+const brushData = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.1.0.json",
+    "data": { "url": "https://sbro0020.github.io/data/brush.csv" },
+    "vconcat": [{
+        "width": containerWidth,
+        "height": containerHeight*0.8,
+        "mark": "area",
+        "encoding": {
+            "x": {
+                "field": "year",
+                "type": "quantitative",
+                "scale": { "domain": { "param": "brush" } },
+                "axis": { "title": "", "tickCount": 10 }
+            },
+            "y": { 
+                "field": "num", 
+                "type": "quantitative",
+                "scale": { "domain": [0, 3000], "nice": false}
+            }
+        }
+    }, {
+        "width": containerWidth,
+        "height": containerHeight*0.2,
+        "mark": "area",
+        "params": [{
+            "name": "brush",
+            "select": { "type": "interval", "encodings": ["x"] }
+        }],
+        "encoding": {
+            "x": {
+                "field": "year",
+                "title": "Year (CE)",
+                "type": "quantitative",
+                "axis": { "tickCount": 10 }
+            },
+            "y": {
+                "field": "num",
+                "type": "quantitative",
+                "axis": { "tickCount": 3, "grid": false },
+                "scale": { "domain": [0,3000] }
+            }
+        }
+    }]
+}
+
+vegaEmbed('#brush',brushData);
+console.log("Brush embeded");
+    
---
+++ js/density.js
@@ -0,0 +1,96 @@
+var styles = window.getComputedStyle(document.getElementById('density'))
+var containerWidth = parseFloat(styles.width);
+var containerHeight = parseFloat(styles.height);
+
+const densityData = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.1.0.json",
+    "title": "",
+    "width": containerWidth,
+    "height": containerHeight,
+    "data": { "url": "data/density.json" },
+    "transform": [
+        { "density": "age", "bandwidth": 1 }
+    ],
+    "layer": [
+        {
+            "mark": "area",
+            "encoding": {
+                "x": {
+                    "field": "value",
+                    "title": "Age at Death",
+                    "type": "quantitative",
+                    "scale": { "domain": [0, 120] },
+                    "axis": {
+                        "titleFont": "Lucida Sans Unicode",
+                        "titleFontWeight": 600,
+                        "labelFont": "Lucida Sans Unicode",
+                        "labelFontWeight": 200
+                    }
+                },
+                "y": {
+                    "field": "density",
+                    "title": "Density of Mathematicians",
+                    "type": "quantitative",
+                    "scale": { "domain": [0,0.035] },
+                    "axis": {
+                        "titleFont": "Lucida Sans Unicode",
+                        "titleFontWeight": 600,
+                        "labelFont": "Lucida Sans Unicode",
+                        "labelFontWeight": 200
+                    }
+                },
+                "color": { "value": "#377eb8" },
+                "tooltip": [
+                    { "field": "value", "type": "quantitative", "title": "Age" },
+                    { "field": "density", "type": "quantitative", "title": "Density" }
+                ]
+            }
+        },{
+            "mark": "rule",
+            "encoding": {
+                "x": {
+                    "aggregate": "mean",
+                    "field": "value",
+                    "type": "quantitative",
+                    "scale": { "domain": [0, 120] }
+                },
+                "color": { "value": "#e41a1c" },
+                "size": { "value": 3 },
+                "tooltip": [
+                    { "aggregate": "mean", "field": "value", "type": "quantitative", "title": "Average Life Span" }
+                ]
+            }
+        },{
+            "mark": {
+                "type": "text",
+                "align": "left",
+                "dx": 5,
+                "dy": -190,
+                "fontSize": 12,
+                "fontWeight": 300
+            },
+            "encoding": { 
+                "x": { 
+                    "aggregate": "mean",
+                    "field": "value",
+                    "type": "quantitative",
+                    "scale": { "domain": [0,120] }
+                },
+                "text": { 
+                    "value": "Average",
+                    "format": {
+                        "font": "Lucida Sans Console",
+                        "text": "Average"
+                    }
+                },
+                "tooltip": [
+                    { "aggregate": "mean", "field": "value", "type": "quantitative", "title": "Average Life Span" }
+                ]
+            }
+        }
+    ]
+}
+
+vegaEmbed('#density',densityData);
+console.log("Density embeded");
+    
