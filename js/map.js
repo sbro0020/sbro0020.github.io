31197ade4f9a32b160ed7781729291ed7bd6ac26
Byrix <brosea101@gmail.com>
2021-10-16 18:42:05 +1100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@ console.log(containerWidth);  //568
 
 const mapData = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.1.0.json",
-    "width": containerWidth*568/636,
+    "width": (containerWidth-9)*568/636,
     "height": containerHeight*288283/298000,
     "data": {
         "url": "data/worldMap.topojson",
